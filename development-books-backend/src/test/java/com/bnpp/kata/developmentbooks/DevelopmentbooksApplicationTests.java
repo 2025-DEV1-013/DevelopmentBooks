81a4de08286945e4302452e240818067d5dfec9e
2025-DEV1-013 <my.developer.backend@gmail.com>
2025-12-08 02:59:13 +0530

    chore: initial Spring Boot project setup
    Created basic backend structure with main app class, test class, application.properties and pom.xml to prepare for TDD development.
    
    Closes #1

---
+++ development-books-backend/src/test/java/com/bnpp/kata/developmentbooks/DevelopmentbooksApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bnpp.kata.developmentbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // default implementation ignored
+	}
+
+}
