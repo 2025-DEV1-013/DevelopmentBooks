5abd9fa135e45cf997d9add81455953eca9c1d8f
2025-DEV1-013 <my.developer.backend@gmail.com>
2025-12-09 11:17:07 +0530

    docs: add OpenAPI UI support and enhance controller documentation
    Added the openapi-starter-webmvc-ui dependency and updated the controller with @Tag and @Operation annotations for API documentation. Also enabled logging by adding the @Slf4j annotation.
    
    Closes #8

--- development-books-backend/src/main/java/com/bnpp/kata/developmentbooks/controller/BookController.java
+++ development-books-backend/src/main/java/com/bnpp/kata/developmentbooks/controller/BookController.java
@@ -3,7 +3,10 @@ package com.bnpp.kata.developmentbooks.controller;
 
 import com.bnpp.kata.developmentbooks.dto.BookResponse;
 import com.bnpp.kata.developmentbooks.service.BookService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/books")
+@Tag(name = "Book API", description = "APIs for fetching development books")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
 
+    @Operation(summary = "Fetch all books", description = "Returns all development books stored in enum")
     @GetMapping("/getbooks")
     public ResponseEntity<List<BookResponse>> getAllBooks() {
+        log.info("Hit endpoint : GET /api/books/getbooks");
         return ResponseEntity.ok(bookService.getAllBooks());
     }
 }
--- development-books-backend/src/main/java/com/bnpp/kata/developmentbooks/service/BookService.java
+++ development-books-backend/src/main/java/com/bnpp/kata/developmentbooks/service/BookService.java
@@ -2,15 +2,18 @@ package com.bnpp.kata.developmentbooks.service;
 
 import com.bnpp.kata.developmentbooks.dto.BookResponse;
 import com.bnpp.kata.developmentbooks.store.BookEnum;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Stream;
 
+@Slf4j
 @Service
 public class BookService {
 
     public List<BookResponse> getAllBooks() {
+        log.info("Retrieving all available books");
         return Stream.of(BookEnum.values())
                 .map(book -> new BookResponse(book.id, book.title, book.author, book.year, book.price))
                 .toList();
