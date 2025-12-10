72f863ff8b3388d6a48f151ddce36bfcefbb0530
2025-DEV1-013 <my.developer.backend@gmail.com>
2025-12-10 08:05:35 +0530

    fix: update code in Bookcard to pass test cases.
    Closes #36

--- development-books-frontend/src/components/BookCard.jsx
+++ development-books-frontend/src/components/BookCard.jsx
@@ -1,21 +1,5 @@
 import "../styles/BookCard.css";
 
-export default function BookCard({ book }) {
-  return (
-    <div className="book-card">
-      <img
-        src={book.imageUrl}
-        alt={book.title}
-        className="book-image"
-      />
-
-      <h2 className="book-title">{book.title}</h2>
-      <p className="book-author">{book.author}</p>
-      <p className="book-price">€{book.price}</p>
-    </div>
-  );
-}
-
 export default function BookCard({ book, quantity, increment }) {
   return (
     <div className="book-card">
